959f31aee00296cd82745f02acae898bca03f7b5
randomtask010 <rory.mcc.01@gmail.com>
2015-05-05 21:20:22 +1000

    Camera Change Thing in the walkthrough scene
    Press enter to trigger the change. still have to build in the other
    triggers. thinking text boxes

---
+++ cube-commander-unity-4/Assets/scripts/CameraChange.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraChange : MonoBehaviour {
+	public Camera camera;
+	public Camera camera2;
+	public Camera camera3;
+	public Camera camera4;
+	public Camera camera5;
+
+
+	void Start() {
+		camera.enabled = true;
+		camera2.enabled = false;
+		camera3.enabled = false;
+		camera4.enabled = false;
+		camera5.enabled = false;
+
+	}
+	
+	void Update() {
+		//This will toggle the enabled state of the two cameras between true and false each time
+		int currentCameraVar = 0; 
+
+			if (Input.GetKeyUp(KeyCode.Return)) {
+				camera.enabled = !camera.enabled;
+				camera2.enabled = !camera2.enabled;
+			}
+
+		}
+
+
+}
